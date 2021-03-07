33e7a77050a5a47bda87cd22e02c8b9b55b3cf7c
José <mailjmq@gmail.com>
2021-03-07 11:47:15 -0300

    disable buttons  in other non-authorized sites

--- popup.js
+++ popup.js
@@ -1,24 +1,27 @@
 console.log('popup.js');
 
+let params = {
+	active: true,
+	currentWindow: true,
+};
 const sendMessage = e => {
 	console.log(e);
 	let msg = { txt: e.target.value };
-	let params = {
-		active: true,
-		currentWindow: true,
-	};
 	chrome.tabs.query(params, gotTabs);
 	function gotTabs(tabs) {
-		console.log(tabs[0].id, msg);
-		chrome.tabs.sendMessage(tabs[0].id, msg);
+		if (
+			tabs[0].url.indexOf('https://www2.personas.santander.com.ar/') > -1
+		) {
+			chrome.tabs.sendMessage(tabs[0].id, msg);
+		} else {
+			console.log('Wrong site!');
+		}
 	}
 };
 
 document.querySelectorAll('button').forEach(btn => {
 	btn.addEventListener('click', sendMessage);
 });
-
-// chrome.browserAction.onClicked.addListener(sendMessage);
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	let data = msg.data;
@@ -35,3 +38,14 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 	// 	responde: 'descargado',
 	// });
 });
+
+chrome.tabs.query(params, gotTabs);
+function gotTabs(tabs) {
+	if (tabs[0].url.indexOf('https://www2.personas.santander.com.ar/') === -1) {
+		document
+			.querySelectorAll('button')
+			.forEach(btn => (btn.disabled = true));
+	} else {
+		console.log('enable');
+	}
+}
